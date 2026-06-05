67fccf72617f63837c0d704b082c40140341a507
Lutrick <m.lutrick@mitsui.com>
2026-06-05 12:50:49 -0500

    more css attempts

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import App from './app'
 import './app.tailwind.css'
+import App from './app'
+import './frame-reset.css'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
